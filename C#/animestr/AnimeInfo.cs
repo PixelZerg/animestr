58b018071b2947a04ce446fca09d72444e793e9c
Abhinav Bhandari <abhinavbhandari4@gmail.com>
2016-09-29 20:38:11 +0100

    Add: AnimeInfo class

---
+++ C#/animestr/AnimeInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace animestr
+{
+    public class AnimeInfo
+    {
+        public string description = null;
+        public string alts = null;
+        public List<string> genres = new List<string>();
+
+        //MAL
+        public string score = null;
+        public string rank = null;
+        public string popularity = null;
+
+        public bool usedMAL { get; private set; }
+    }
+}
--- C#/animestr/Program.cs
+++ C#/animestr/Program.cs
@@ -3,85 +3,7 @@ using System.Net;
 
 namespace animestr
 {
-	class MainClass
-	{
-		public static void Main (string[] args)
-		{
-			//Console.WriteLine (Console.WindowWidth+"x"+Console.WindowHeight);
-			ConsoleList list = new ConsoleList("Mooo list",
-				"moo", 
-				"loo", 
-				"boo",
-				"foo",
-				"ijdsj", 
-				"sdh",
-				"hdsi",
-				"dio",
-				"iasjdj",
-				"dsjfidshhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhjf",
-				"sadijij",
-				"sad",
-				"iei",
-				"dsifj",
-				"djfsijfd",
-				"aosd",
-				"asdjiod",
-				"jsdfdfs",
-				"asdji",
-				"oadsoe03",
-				"ii3",
-				"i3",
-				"i5",
-				"i8",
-				"ujiij",
-				"a0",
-				"a1",
-				"a2",
-				"a3",
-				"a4",
-				"a5",
-				"a6",
-				"a7",
-				"a8",
-				"a9",
-				"a10",
-				"a11",
-				"a12",
-				"a13",
-				"a14",
-				"a15",
-				"a16",
-				"a17",
-				"a18",
-				"a19",
-				"a20",
-				"a21",
-				"a22",
-				"a23",
-				"a24",
-				"a25",
-				"a26",
-				"a27",
-				"a28",
-				"a29",
-				"a30",
-				"a31",
-				"a32",
-				"a33",
-				"a34",
-				"a35",
-				"a36",
-				"a37"
-			);
-
-			list.PrintList("Wowie wow wow",1);
-			Console.ReadKey();
-			Utils.ClearConsole();
-			list.PrintList("Wowie wodsfhusdhfw wow", 2);
-			Console.Write("MOOO: ");
-			Console.ReadLine();
-
-			/* TODO:
+    /* TODO:
 			 * Read first character, establish what is being inputed from that character and ask for the rest of the input (like "MOOO:" above)
 			 * Make the searching system
 			 * Make a source selection system with indicators to show if the source can be reached, is working, etc...
@@ -89,6 +11,11 @@ namespace animestr
 			 * Show a recommended anime list on startup
 			 * Make a cool ascii art splash screen
 			*/
+
+    class MainClass
+	{
+		public static void Main (string[] args)
+		{
 		}
     }
 }
