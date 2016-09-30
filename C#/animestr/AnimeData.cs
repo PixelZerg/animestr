ce7668ff6bb7f354ef1240e74abf79f70c495334
Abhinav Bhandari <abhinavbhandari4@gmail.com>
2016-09-30 19:26:02 +0100

    Changed AnimeData structure

--- C#/animestr/AnimeData.cs
+++ C#/animestr/AnimeData.cs
@@ -9,30 +9,7 @@ namespace animestr
     public class AnimeData
     {
         public AnimeEntry entry = null;
-        private AnimeInfo _malInfo = null;
-        public AnimeInfo malInfo
-        {
-            get
-            {
-                if (_malInfo != null)
-                {
-                    return _malInfo;
-                }
-                else
-                {
-                    if (entry != null)
-                    {
-                        if (entry.name != null)
-                        {
-                            _malInfo = new AnimeInfo(entry.name);
-                            return malInfo;
-                        }
-                    }
-                }
-                return null;
-            }
-            set { _malInfo = value; }
-        }
+        public AnimeInfo info = null;
         public List<EpisodeData> episodes = null; //do not instantiate on initialise - differentiate between no episodes and not parsed
     }
 }
