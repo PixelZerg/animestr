d8d91961ca04bac8382e0d85a0457a224ab044b8
Abhinav Bhandari <abhinavbhandari4@gmail.com>
2016-09-30 20:49:06 +0100

    Begin Add: AnimeDao

---
+++ C#/animestr/Sources/AnimeDao.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace animestr.Sources
+{
+    public class AnimeDao : IAnimeSource
+    {
+        public AnimeData GetData(AnimeEntry entry)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, string> GetMRLs(string epPageUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<AnimeEntry> GetRecommendations()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<AnimeEntry> GetSearchResult(string query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
