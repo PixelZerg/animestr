f8798c193a6778414698ff8eb993b1c4eada4d04
Abhinav Bhandari <abhinavbhandari4@gmail.com>
2016-10-17 18:50:46 +0000

    added some stuff

--- C#/animestr/Views/EpisodesView.cs
+++ C#/animestr/Views/EpisodesView.cs
@@ -7,10 +7,22 @@ namespace animestr
         public Display display = null;
         public EpisodeData data = null;
 
+        public ConsoleList clist = null;
+
         public EpisodesView(Display display, EpisodeData data)
         {
             this.display = display;
             this.data = data;
+        }
+
+        public void Show()
+        {
+            
+        }
+
+        public void ShowData()
+        {
+            
         }
     }
 }
