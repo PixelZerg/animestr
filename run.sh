5a36664e15c9e5ac8c25f064a5d7989c89f78b97
Abhinav Bhandari <abhinavbhandari4@gmail.com>
2016-10-17 17:59:51 +0000

    removed auto commit

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~/workspace/
 xbuild C#/animestr.sln
-git add *
-git commit -m "updates (auto)"
-git push origin master
+#git add *
+#git commit -m "updates (auto)"
+#git push origin master
 mono C#/animestr/bin/Debug/animestr.exe
